dcebba40fff9d9167cf5cb04bae9243945a1fb39
Kevin Bell <bellKevin@pm.me>
2022-09-09 10:00:04 -0600

    Update SolutionJUnitTest.java

--- codeWars/test/SolutionJUnitTest.java
+++ codeWars/test/SolutionJUnitTest.java
@@ -12,5 +12,9 @@ public class SolutionJUnitTest {
     public void testSomething() {
         assertEquals(16, Solution.areaOrPerimeter(4, 4));
         assertEquals(32, Solution.areaOrPerimeter(6, 10));
+        assertEquals(64, Solution.areaOrPerimeter(8, 8));
+        assertEquals(64, Solution.areaOrPerimeter(12, 20));
+        assertEquals(128, Solution.areaOrPerimeter(24, 40));
+        assertNotEquals(127, Solution.areaOrPerimeter(24, 40));
     } // end testSomething method
 } // end SolutionJUnitTest class
