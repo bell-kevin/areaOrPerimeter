605578a2ceb0bfef1c8bfbf1fe6d684d67139d43
Kevin Bell <bellKevin@pm.me>
2022-09-09 10:51:43 -0600

    Update SolutionJUnitTest.java

--- codeWars/test/SolutionJUnitTest.java
+++ codeWars/test/SolutionJUnitTest.java
@@ -1,12 +1,51 @@
+/**
+ * ****************************************************************************
+ * SolutionJUnitTest.java
+ * Kevin Bell
+ *
+ * This program unit tests area or perimeter values.
+ * ***************************************************************************
+ */
+package codeWars;
 
-import codeWars.Solution;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.*;
 import org.junit.Test;
 
+@RunWith(Parameterized.class)
 public class SolutionJUnitTest {
 
     public SolutionJUnitTest() {
     }
+    //length
+    @Parameterized.Parameter(0)
+    public int l;
+    //width
+    @Parameterized.Parameter(1)
+    public int w;
+    //expected result
+    @Parameterized.Parameter(2)
+    public double result;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][]{
+            {16, 4, 40},
+            {32, 6, 76},
+            {64, 8, 144},
+            {64, 12, 152},
+            {128, 24, 304}
+        };
+        return Arrays.asList(data);
+    } // end method
+
+    @Test
+    public void testEverything() {
+        double actual = Solution.areaOrPerimeter(l, w);
+        assertEquals(result, actual, 0.01);
+    } // end testEverything method
 
     @Test
     public void testSomething() {
@@ -16,5 +55,5 @@ public class SolutionJUnitTest {
         assertEquals(64, Solution.areaOrPerimeter(12, 20));
         assertEquals(128, Solution.areaOrPerimeter(24, 40));
         assertNotEquals(127, Solution.areaOrPerimeter(24, 40));
-    } // end testSomething method
+    } // end testSomething method 
 } // end SolutionJUnitTest class
